224f5f956ce59080982deb8c5882c602371e9f6e
Zara477 <zarababar112@gmail.com>
2026-06-09 16:56:56 +0500

    Update billing.php

--- billing.php
+++ billing.php
@@ -24,7 +24,7 @@ if(isset($_POST['add_bill']))
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Billing Management</title>
+<title> Billing Management </title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
