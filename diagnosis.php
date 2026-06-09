97be6a67f2bbb7d5b4c286deea832d4c57f78730
Zara477 <zarababar112@gmail.com>
2026-06-09 16:57:30 +0500

    Update diagnosis.php

--- diagnosis.php
+++ diagnosis.php
@@ -28,7 +28,7 @@ if(isset($_POST['add_diagnosis']))
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Diagnosis Management</title>
+<title> Diagnosis Management </title>
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="style.css">
